508d47b9d722885c24d638f175f2619933649e1d
Narges Nagy <nargesnagy21@gmail.com>
2023-02-11 04:26:09 +0200

    adding mealsviewmodel and manifist file update

---
+++ app/src/main/java/com/example/cleanarchitectureapp/home/viewmodel/MealsViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.cleanarchitectureapp.home.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.models.CategoryResponse
+import com.example.domain.usecase.GetMealsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MealsViewModel @Inject constructor(
+private val getMealsUseCase: GetMealsUseCase
+) : ViewModel(){
+
+    private val _categoryResponse : MutableStateFlow<CategoryResponse?> = MutableStateFlow(null)
+    val categoryResponse : StateFlow<CategoryResponse?> = _categoryResponse
+
+    fun getMealsFromRemote(){
+        viewModelScope.launch {
+            try{
+                _categoryResponse.value = getMealsUseCase()
+            } catch (e : Exception){
+                Log.e("TAG ViewModel" , e.message.toString())
+            }
+
+        }
+    }
+}
